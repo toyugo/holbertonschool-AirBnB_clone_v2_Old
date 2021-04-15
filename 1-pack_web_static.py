f716ceb867b09f59abb38fa7be096c7b9dc7f49b
Toyugo <2490@holbertonschool.com>
2021-04-15 14:48:29 +0000

    fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,12 +7,12 @@ import os.path
 
 def do_pack():
     """ pack my static"""
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    timestamp = time.strftime("%Y%m%d%H%M%S")
     path = "versions/web_static_{}.tgz".format(timestamp)
     try:
         if not os.path.exists('versions'):
-            l = local("mkdir -p versions")
+            local("mkdir -p versions")
         local("tar -cvzf {} web_static".format(path))
         return path
-    except:
+    except Exception:
         return None
